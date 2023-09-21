bb67f7d5930c000ef2a0b65ce63a9e27e0b007a4
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-09-21 21:33:02 +0330

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,30 +1,31 @@
 from django.contrib import admin
-from .models import NewUser
 from django.contrib.auth.admin import UserAdmin
-from django.forms import TextInput, Textarea
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 
-class UserAdminConfig(UserAdmin):
-    model = NewUser
-    search_fields = ('email', 'user_name', 'first_name',)
-    list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
-    ordering = ('-start_date',)
-    list_display = ('email', 'user_name', 'first_name',
-                    'is_active', 'is_staff')
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
-        (None, {'fields': ('email', 'user_name', 'first_name',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-        ('Personal', {'fields': ('about',)}),
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
-    formfield_overrides = {
-        NewUser.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
-    }
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')}
-         ),
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
     )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
-admin.site.register(NewUser, UserAdminConfig)
+admin.site.register(CustomUser, CustomUserAdmin)
