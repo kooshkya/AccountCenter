aa6701b966e52db0cc14997ed13dc1f4f51934ef
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-09-21 22:23:22 +0330

    refactored some names

--- users/models.py
+++ users/models.py
@@ -2,17 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from .managers import CustomUserManager
+from .managers import UserManager
 
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     def __str__(self):
         return self.email
